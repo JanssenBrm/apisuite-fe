b63adefb5e7584a7ea1e27ce5e6f92f4cedaada5
Joao F E <joaoeirinh@gmail.com>
2019-12-10 14:25:55 +0000

    feat(login-window): exits login and registration windows with escape key

--- packages/apisuite-client-sandbox/src/containers/Register/Register.tsx
+++ packages/apisuite-client-sandbox/src/containers/Register/Register.tsx
@@ -7,6 +7,7 @@ import IconButton from '@material-ui/core/IconButton'
 import Visibility from '@material-ui/icons/Visibility'
 import VisibilityOff from '@material-ui/icons/VisibilityOff'
 import Shuffle from '@material-ui/icons/Shuffle'
+import { Redirect } from 'react-router'
 
 const portalRoot = document.getElementById('portal-root')    
 
@@ -18,73 +19,106 @@ const Register: React.FC<{}> = () => {
     const registerTitle = "Registration"
     const registerButtonLabel = "Confirm"
     const closeRoute = "/"
+    const escapeKeyPress = useKeyPress('Escape')
 
-    function handleClickShowPassword(){
+    function handleClickShowPassword () {
         setShowPassword(!showPassword)
     }
 
-    return ReactDom.createPortal(
-        <div className={classes.registerContainer}>
-            <div className={classes.content}>
-                <FormCard title={registerTitle} buttonLabel={registerButtonLabel} closeRoute={closeRoute} >
-                    <div className={classes.fieldContainer}>
-                        <h5 className={classes.fieldTitle}>GDPR Protected</h5>
-                        <TextField
-                          placeholder='Your name'
-                          variant='outlined'
-                          margin='none'
-                          type='email'
-                          autoFocus
-                          fullWidth
-                          InputProps={{
-                              classes: { input: classes.emailTextfield },
-                          }}
-                        />
-                    </div>
-                    <div className={classes.fieldContainer}>
-                        <h5 className={classes.fieldTitle}>E-mail address</h5>
-                        <TextField
-                        placeholder='example@cloudoki.com'
-                        variant='outlined'
-                        margin='none'
-                        type='email'
-                        autoFocus
-                        fullWidth
-                        InputProps={{
-                            classes: { input: classes.emailTextfield },
-                        }}
-                        />
-                    </div>
-                    <div className={classes.fieldContainer}>
-                        <h5 className={classes.fieldTitle}>Pass Phrase</h5>
-                        <div className={classes.passPhraseContainer}>
+    if (portalRoot !== null) {
+        return ReactDom.createPortal(
+            escapeKeyPress ?
+            <Redirect to={closeRoute} />
+            :
+            <div className={classes.registerContainer}>
+                <div className={classes.content}>
+                    <FormCard title={registerTitle} buttonLabel={registerButtonLabel} closeRoute={closeRoute} >
+                        <div className={classes.fieldContainer}>
+                            <h5 className={classes.fieldTitle}>GDPR Protected</h5>
                             <TextField
+                            placeholder='Your name'
                             variant='outlined'
                             margin='none'
-                            type={showPassword ? 'text' : 'password'}
+                            type='email'
                             autoFocus
                             fullWidth
                             InputProps={{
-                                classes: { input: classes.passPhrasefield },
+                                classes: { input: classes.emailTextfield },
                             }}
                             />
-                            <div className={classes.btnsContainer}>
-                                <IconButton
-                                  onClick={handleClickShowPassword}
-                                >
-                                    {showPassword ? <VisibilityOff /> : <Visibility />}
-                                </IconButton>
-                                <IconButton>
-                                    <Shuffle />
-                                </IconButton>
+                        </div>
+                        <div className={classes.fieldContainer}>
+                            <h5 className={classes.fieldTitle}>E-mail address</h5>
+                            <TextField
+                            placeholder='example@cloudoki.com'
+                            variant='outlined'
+                            margin='none'
+                            type='email'
+                            fullWidth
+                            InputProps={{
+                                classes: { input: classes.emailTextfield },
+                            }}
+                            />
+                        </div>
+                        <div className={classes.fieldContainer}>
+                            <h5 className={classes.fieldTitle}>Pass Phrase</h5>
+                            <div className={classes.passPhraseContainer}>
+                                <TextField
+                                variant='outlined'
+                                margin='none'
+                                type={showPassword ? 'text' : 'password'}
+                                fullWidth
+                                InputProps={{
+                                    classes: { input: classes.passPhrasefield },
+                                }}
+                                />
+                                <div className={classes.btnsContainer}>
+                                    <IconButton
+                                    onClick={handleClickShowPassword}
+                                    >
+                                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                                    </IconButton>
+                                    <IconButton>
+                                        <Shuffle />
+                                    </IconButton>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </FormCard>
-            </div>
-        </div>,
-        portalRoot
-    )
+                    </FormCard>
+                </div>
+            </div>,
+        portalRoot)
+    } else {
+        return <Redirect to={closeRoute} />
+    }
 } 
+
+function useKeyPress (targetKey: string) {
+    const [keyPressed, setKeyPressed] = React.useState(false)
+
+    const keyDownHandler = (e: KeyboardEvent) => {
+        if (e.key === targetKey){
+            setKeyPressed(true)
+        }
+    }
+
+    const keyUpHandler = (e: KeyboardEvent) => {
+        if (e.key === targetKey){
+            setKeyPressed(false)
+        }
+    }
+
+    React.useEffect(() => {
+        document.addEventListener('keydown', keyDownHandler)
+        document.addEventListener('keyup', keyUpHandler)
+
+        return () => {
+            document.removeEventListener('keydown', keyDownHandler)
+            document.removeEventListener('keyup', keyUpHandler)
+        }
+    }, [])
+    
+    return keyPressed
+}
 
 export default Register
