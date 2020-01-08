de2c8e2cd1635c16c5eb1967f7e1cc7750bc85a2
Joao F E <joaoeirinh@gmail.com>
2020-01-08 12:00:57 +0000

    chore: removed logs

--- packages/apisuite-client-sandbox/src/containers/App/App.tsx
+++ packages/apisuite-client-sandbox/src/containers/App/App.tsx
@@ -64,8 +64,6 @@ const App: React.FC<AppProps> = ({ user, history }) => {
 
   return (
     <ThemeProvider theme={theme}>
-      {console.log(process.env.API_URL)}
-      {console.log(process.env.NODE_ENV)}
       <Navigation
         key='app-navigation'
         tabs={tabs.map((tab) => tab.label)}
