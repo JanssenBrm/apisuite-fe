ae0438c8a5ad1081682ac05f2fb52400d01b3982
Joao F E <joaoeirinh@gmail.com>
2020-01-23 17:06:44 +0000

    chore(RequireAuth): remove unnecessary div

--- packages/apisuite-client-sandbox/src/containers/Auth/RequireAuth.tsx
+++ packages/apisuite-client-sandbox/src/containers/Auth/RequireAuth.tsx
@@ -3,7 +3,5 @@ import { RequireAuthProps } from './types'
 import { Redirect } from 'react-router'
 
 export const RequireAuth: React.FC<RequireAuthProps> = ({ component: Component, user, ...rest }) => (
-  <div>
-    {user ? <Component {...rest} /> : <Redirect to='/login' />}
-  </div>
+  user ? <Component {...rest} /> : <Redirect to='/login' />
 )
