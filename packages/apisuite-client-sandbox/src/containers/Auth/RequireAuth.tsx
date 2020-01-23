bfcceec6447521c8ba0bbe7a208f68c841214504
Joao F E <joaoeirinh@gmail.com>
2020-01-23 14:48:23 +0000

    chore(RequireAuth): remove logs and comments

--- packages/apisuite-client-sandbox/src/containers/Auth/RequireAuth.tsx
+++ packages/apisuite-client-sandbox/src/containers/Auth/RequireAuth.tsx
@@ -5,6 +5,5 @@ import { Redirect } from 'react-router'
 export const RequireAuth: React.FC<RequireAuthProps> = ({ component: Component, user, ...rest }) => (
   <div>
     {user ? <Component {...rest} /> : <Redirect to='/login' />}
-    {console.log(user)}
   </div>
 )
--- packages/apisuite-client-sandbox/src/containers/Auth/index.ts
+++ packages/apisuite-client-sandbox/src/containers/Auth/index.ts
@@ -1,7 +1,6 @@
 import { RequireAuth } from './RequireAuth'
 import { Store } from 'store/types'
 import { connect } from 'react-redux'
-// import { RequireAuthStateProps, RequireAuthOwnProps } from './types'
 
 const mapStateToProps = ({ auth }: Store) => ({
   user: auth.user,
