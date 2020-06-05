64fa47a2ddab48838a2cc13f8fde7edb3e9c283d
Joao F E <joaoeirinh@gmail.com>
2020-06-05 10:40:33 +0100

    added ducks and sagas

--- packages/apisuite-client-sandbox/src/containers/Profile/ducks.ts
+++ packages/apisuite-client-sandbox/src/containers/Profile/ducks.ts
@@ -1,24 +1,45 @@
-// import update from 'immutability-helper'
-// import {
-//   ProfileActions,
-//   ProfileStore,
-// } from './types'
+import update from 'immutability-helper'
+import {
+  ProfileActions,
+  ProfileStore,
+} from './types'
 
-// const initialState: ProfileStore = {
+const initialState: ProfileStore = {
+  teamMembers: [],
+}
 
-// }
+export enum ProfileActionTypes {
+  FETCH_TEAM_MEMBERS_REQUEST = 'FETCH_TEAM_MEMBERS_REQUEST',
+  FETCH_TEAM_MEMBERS_SUCCESS = 'FETCH_TEAM_MEMBERS_SUCCESS',
+  FETCH_TEAM_MEMBERS_ERROR = 'FETCH_TEAM_MEMBERS_ERROR'
+}
 
-// export enum ProfileActionTypes {
+export default function profileReducer (
+  state: initialSate,
+  action: ProfileActions,
+): ProfileStore {
+  switch (action.type) {
+    case 
+  }
+}
 
-// }
-
-// export default function profileReducer (
-//   state: initialSate,
-//   action: ProfileActions,
-// ): ProfileStore {
-//   switch (action.type) {
-//     case
-//   }
-// }
-
-// export const 
+export const fetchTeamMembersActions = {
+  request: () => {
+    return {
+      type: ProfileActionTypes.FETCH_TEAM_MEMBERS_REQUEST,
+      payload: ,
+    } as const
+  },
+  success: (response: PersonalDetailsResponse) => {
+    return {
+      type: ProfileActionTypes.FETCH_TEAM_MEMBERS_SUCCESS,
+      response: response,
+    } as const
+  },
+  error: (error: string) => {
+    return {
+      type: ProfileActionTypes.FETCH_TEAM_MEMBERS_ERROR,
+      error: error,
+    } as const
+  },
+}
