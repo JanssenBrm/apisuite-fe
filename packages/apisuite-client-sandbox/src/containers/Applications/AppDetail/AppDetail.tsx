d950c88ddde657ea9f5d1b0144b39be9bd3f8d19
Pedro Cloudoki <pedro@cloudoki.com>
2020-09-28 16:14:16 +0100

    chore: Removed unnecessary comments

--- packages/apisuite-client-sandbox/src/containers/Applications/AppDetail/AppDetail.tsx
+++ packages/apisuite-client-sandbox/src/containers/Applications/AppDetail/AppDetail.tsx
@@ -173,7 +173,6 @@ const AppDetail: React.FC<AppDetailProps> = (
   }
 
   function handleInputs (e: FormFieldEvent, err: any) {
-    // If the BE gives us an empty 'pubUrls' (i.e., '[]'), we have to set up our own
     const newPubUrls = currentInput.pubUrls.length > 0 ?
     [...currentInput.pubUrls] :
     [
