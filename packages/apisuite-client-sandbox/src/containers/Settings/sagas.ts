e0b47868fe1f24e4f312d03a3be890771e605a46
Tiago Alves <tiagoadalves@gmail.com>
2020-10-08 10:26:58 +0100

    fix(Settings): Remove the `x-access-token` header when reading from the Settings API

--- packages/apisuite-client-sandbox/src/containers/Settings/sagas.ts
+++ packages/apisuite-client-sandbox/src/containers/Settings/sagas.ts
@@ -1,22 +1,14 @@
 import { GET_SETTINGS, getSettingsSuccess } from './ducks'
-import { takeLatest, call, put, select } from 'redux-saga/effects'
+import { takeLatest, call, put } from 'redux-saga/effects'
 import { API_URL, SIGNUP_PORT } from 'constants/endpoints'
 import request from 'util/request'
-import { Store } from 'store/types'
 
 export function * getSettings () {
   try {
     const getSettingsUrl = `${API_URL}${SIGNUP_PORT}/settings`
-    const accessToken = yield select(
-      (state: Store) => state.auth.authToken,
-    )
-
     const response = yield call(request, {
       url: getSettingsUrl,
       method: 'GET',
-      headers: {
-        'x-access-token': accessToken,
-      },
     })
 
     yield put(getSettingsSuccess(response))
