a991632b9b89dbdc2709f611590303a0c2161eb7
Pedro Cloudoki <pedro@cloudoki.com>
2020-11-26 15:44:54 +0000

    chore: Replaced all 'Sandbox' references from the 'Test' page by using the term 'API' instead.

--- packages/apisuite-client-sandbox/src/containers/Instructions/styles.ts
+++ packages/apisuite-client-sandbox/src/containers/Instructions/styles.ts
@@ -3,102 +3,119 @@ import { config } from 'constants/global'
 
 export default makeStyles(({
   root: {
+    backgroundColor: config.palette.background.default,
     minHeight: '100%',
-    backgroundColor: '#E3E3E3',
   },
+
   contentContainer: {
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'space-around',
-    maxWidth: 900,
     margin: '0 auto',
+    maxWidth: 900,
     transform: 'translateX(-8px)',
   },
+
   mainContainer: {
     display: 'flex',
     flexDirection: 'row',
     marginBottom: 100,
     width: '100%',
   },
+
   content: {
     display: 'flex',
     flexDirection: 'column',
     width: 688,
   },
+
   navigation: {
     display: 'flex',
     flexDirection: 'column',
     width: 300,
   },
+
   title: {
     fontSize: 26,
     fontWeight: 300,
   },
+
   stepTitle: {
     fontSize: 20,
     marginTop: -170,
     paddingTop: 170,
   },
+
   description: {
     display: 'flex',
-    textAlign: 'justify',
-    padding: 0,
     marginBottom: 20,
     marginTop: 0,
+    padding: 0,
+    textAlign: 'justify',
   },
+
   stepContainer: {
-    marginTop: 20,
     marginBottom: 20,
+    marginTop: 20,
   },
+
   codeBlock: {
-    borderTopRightRadius: 0,
-    borderTopLeftRadius: 0,
     borderBottomLeftRadius: config.dimensions.borderRadius,
     borderBottomRightRadius: config.dimensions.borderRadius,
+    borderTopRightRadius: 0,
+    borderTopLeftRadius: 0,
   },
+
   noteContainer: {
-    width: '100%',
     backgroundColor: config.palette.secondary,
     borderRadius: config.dimensions.borderRadius,
-    overflow: 'hidden',
     marginBottom: 20,
+    overflow: 'hidden',
+    width: '100%',
   },
+
   noteTitle: {
-    fontSize: 16,
     color: 'black',
+    fontSize: 16,
     fontWeight: 400,
   },
+
   note: {
-    fontSize: 14,
     color: 'white',
+    fontSize: 14,
   },
+
   noteContent: {
     backgroundColor: config.palette.primary,
-    padding: 20,
     marginLeft: 5,
+    padding: 20,
   },
+
   iconRow: {
-    display: 'flex',
-    color: 'white',
-    justifyContent: 'flex-end',
     alignItems: 'flex-end',
-    backgroundColor: '#333333',
+    backgroundColor: config.palette.text.primary,
+    color: 'white',
+    display: 'flex',
     height: 40,
+    justifyContent: 'flex-end',
     marginTop: 0,
     paddingLeft: 20,
     paddingRight: 20,
-    borderTopRightRadius: config.dimensions.borderRadius,
-    borderTopLeftRadius: config.dimensions.borderRadius,
     borderBottomLeftRadius: 0,
     borderBottomRightRadius: 0,
+    borderTopRightRadius: config.dimensions.borderRadius,
+    borderTopLeftRadius: config.dimensions.borderRadius,
   },
+
   clipboardIcon: {
     cursor: 'pointer',
+    zIndex: 1,
+
     '&:hover': {
       color: config.palette.primary,
     },
-    zIndex: 1,
   },
+
   sideMenuContainer: {
     paddingLeft: 40,
     position: 'sticky',
@@ -110,31 +127,37 @@ export const codeStyle = {
   'hljs-string': {
     'color': config.palette.primary,
   },
+
   'hljs-symbol': {
     'color': config.palette.primary,
   },
+
   'hljs-bullet': {
     'color': config.palette.primary,
   },
+
   'hljs-addition': {
     'color': config.palette.primary,
   },
+
   'hljs': {
+    background: config.palette.text.primary,
+    color: '#D8D8D8',
     display: 'block',
-    overflowX: 'auto',
-    background: '#333333',
     fontFamily: 'Roboto, sans-serif',
     fontSize: 14,
-    color: '#D8D8D8',
     marginTop: 0,
-    paddingTop: 0,
+    overflowX: 'auto',
     paddingBottom: 20,
-    paddingRight: 20,
     paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 0,
   },
+
   'hljs-emphasis': {
     fontStyle: 'italic',
   },
+
   'hljs-strong': {
     fontWeight: 'bold',
   },
