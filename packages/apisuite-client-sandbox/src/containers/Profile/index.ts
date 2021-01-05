530d42c0bb1d0051fff9024598da8d926ae57200
Pedro Cloudoki <pedro@cloudoki.com>
2021-01-05 13:55:47 +0000

    feat: Profile view's redesign

--- packages/apisuite-client-sandbox/src/containers/Profile/index.ts
+++ packages/apisuite-client-sandbox/src/containers/Profile/index.ts
@@ -3,7 +3,11 @@ import { connect } from 'react-redux'
 import { bindActionCreators, Dispatch } from 'redux'
 
 import { authActions } from 'containers/Auth/ducks'
-import { getProfileActions, resetErrorAction, updateProfileActions } from 'containers/Profile/ducks'
+import {
+  getProfileActions,
+  resetErrorAction,
+  updateProfileActions,
+} from 'containers/Profile/ducks'
 
 import Profile from './Profile'
 
@@ -11,15 +15,18 @@ import { Store } from 'store/types'
 
 export const mapStateToProps = ({ auth, profile }: Store) => ({
   profile: profile.profile,
-  requestStatutes: profile.requestStatuses,
+  requestStatuses: profile.requestStatuses,
   user: auth.user,
 })
 
-export const mapDispatchToProps = (dispatch: Dispatch) => bindActionCreators({
-  getProfile: getProfileActions.request,
-  logout: () => dispatch(authActions.logout()),
-  resetErrors: resetErrorAction,
-  updateProfile: updateProfileActions.request,
-}, dispatch)
+export const mapDispatchToProps = (dispatch: Dispatch) => bindActionCreators(
+  {
+    getProfile: getProfileActions.request,
+    logout: () => dispatch(authActions.logout()),
+    resetErrors: resetErrorAction,
+    updateProfile: updateProfileActions.request,
+  },
+  dispatch,
+)
 
 export default connect(mapStateToProps, mapDispatchToProps)(Profile)
