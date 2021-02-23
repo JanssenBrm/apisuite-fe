b15ff572278db0d9334df1c1127580f0ce283e95
Diogo Simoes <diogo.miguel.simoes@gmail.com>
2021-02-23 18:18:33 +0000

    chore: cicd configs

--- build.sh
+++ build.sh
@@ -13,28 +13,16 @@ echo ${DOCKER_PASS} | docker login --username ${DOCKER_USER} --password-stdin
 
 HASH=$(git rev-parse --short HEAD)
 
-LATEST=latest
-
-if [ "$CIRCLE_BRANCH" = "staging" ]; then
-  LATEST=stg-latest
-fi
-
-if [ "$CIRCLE_BRANCH" = "develop" ]; then
-  LATEST=dev-latest
-fi
-
-docker build \
-  -t cloudokihub/apisuite-fe:$HASH \
-  -t cloudokihub/apisuite-fe:$LATEST .
-
-docker push cloudokihub/apisuite-fe:$HASH
-docker push cloudokihub/apisuite-fe:$LATEST
-
-
 if [ "$CIRCLE_BRANCH" = "production" ]; then
+
   VERSION=$(cat package.json | grep version | head -1 | awk -F ": " '{ print $2 }' | sed 's/[",]//g')
 
-  docker build -t cloudokihub/apisuite-fe:$VERSION .
+  docker build \
+    -t cloudokihub/apisuite-fe:$HASH \
+    -t cloudokihub/apisuite-fe:latest \
+    -t cloudokihub/apisuite-fe:$VERSION .
+  docker push cloudokihub/apisuite-fe:$HASH
+  docker push cloudokihub/apisuite-fe:latest
   docker push cloudokihub/apisuite-fe:$VERSION
 
   # Cloud image
@@ -45,4 +33,19 @@ if [ "$CIRCLE_BRANCH" = "production" ]; then
 
   docker push cloudokihub/apisuite-fe:cloud-$VERSION
   docker push cloudokihub/apisuite-fe:cloud-latest
+
+else
+
+  LATEST=dev-latest
+  if [ "$CIRCLE_BRANCH" = "staging" ]; then
+    LATEST=stg-latest
+  fi
+
+  docker build \
+    -t cloudokihub/apisuite-fe:$HASH \
+    -t cloudokihub/apisuite-fe:$LATEST .
+
+  docker push cloudokihub/apisuite-fe:$HASH
+  docker push cloudokihub/apisuite-fe:$LATEST
+
 fi
