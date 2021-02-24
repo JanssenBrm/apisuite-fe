152135025a308acb895d47a5631a2a042152bdb9
Diogo Simoes <diogo.miguel.simoes@gmail.com>
2021-02-24 09:27:18 +0000

    chore: new deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-PROJECT_DIR="/apps/apisuite-fe"
+PROJECT_DIR="~/apisuite-setup"
 
-scp -o StrictHostKeyChecking=no ./docker-compose.yaml $SSH_USER@$SSH_HOST:$PROJECT_DIR
-ssh -o StrictHostKeyChecking=no $SSH_USER@$SSH_HOST "cd $PROJECT_DIR && docker-compose --env-file .env pull && docker-compose --env-file .env up -d"
+ENV=dev
+
+if [ "$CIRCLE_BRANCH" = "staging" ]; then
+  ENV=stg
+fi
+
+if [ "$CIRCLE_BRANCH" = "production" ]; then
+  ENV=prd
+fi
+
+ssh -o StrictHostKeyChecking=no $SSH_USER@$SSH_HOST "cd $PROJECT_DIR && ./update-apisuite-fe.sh ${ENV}"
