edb2c4f4974e023ba2155e863ae26dd1455258b6
Diogo Simoes <diogo.miguel.simoes@gmail.com>
2021-03-03 10:45:13 +0000

    fix(tests): fixed run_tests script

--- run_tests.sh
+++ run_tests.sh
@@ -12,8 +12,17 @@ set -e
 
 # . ./install_dependencies.sh
 
-cp "sandbox.config-$CIRCLE_BRANCH.json" "sandbox.config.json"
+CONFIG=develop
+
+if [[ "$CIRCLE_BRANCH" = "staging" || "$CIRCLE_BRANCH" = "production" ]]; then
+  CONFIG=$CIRCLE_BRANCH
+fi
+
+cp "sandbox.config-$CONFIG.json" "sandbox.config.json"
 npm install
 npm run pretest
+
+# NOTE: running pretest only for now (lint/types) since there are still no cypress tests
 # npm run test
+
 rm sandbox.config.json
