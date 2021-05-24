2c856876c49bf287109272557b0dc1e21e63ef32
Bruno Morgado <mstrk512@gmail.com>
2021-05-24 17:03:49 +0100

    refacto(Security)r: use Typography when possible

--- src/pages/Security/Security.tsx
+++ src/pages/Security/Security.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useTranslation, Button, TextField, InputAdornment, IconButton } from "@apisuite/fe-base";
+import { useTranslation, Button, TextField, InputAdornment, IconButton, Typography, Container, Box, Grid } from "@apisuite/fe-base";
 import InfoRoundedIcon from "@material-ui/icons/InfoRounded";
 import VisibilityOffRoundedIcon from "@material-ui/icons/VisibilityOffRounded";
 import VisibilityRoundedIcon from "@material-ui/icons/VisibilityRounded";
@@ -69,26 +69,25 @@ export const Security: React.FC = () => {
 
   return (
     <main className='page-container'>
-      <section className={classes.updatePasswordContainer}>
-        <p className={classes.securityTitle}>
+      <Container maxWidth="md">
+        <Typography variant="h2">
           {t("profileTab.securitySubTab.securityTitle")}
-        </p>
+        </Typography>
 
-        <p className={classes.securitySubtitle}>
-          {
-            !ssoIsActive
-              ? t("profileTab.securitySubTab.securitySubtitleWithoutActiveSSO")
-              : t("profileTab.securitySubTab.securitySubtitleWithActiveSSO")
-          }
-        </p>
+        <Typography variant="body1" color="textSecondary">
+          {!ssoIsActive ? t("profileTab.securitySubTab.securitySubtitleWithoutActiveSSO")
+            : t("profileTab.securitySubTab.securitySubtitleWithActiveSSO")}
+        </Typography>
 
-        <p className={classes.updatePasswordTitle}>
-          {t("profileTab.securitySubTab.updatePasswordTitle")}
-        </p>
+        <Grid container>
+          <Grid item xs>
+            <Box clone mt={3} mb={2}>
+              <Typography variant="h3">
+                {t("profileTab.securitySubTab.updatePasswordTitle")}
+              </Typography>
+            </Box>
 
-        {
-          ssoIsActive
-            ? (
+            {ssoIsActive ? (
               <div className={classes.infoBox}>
                 <InfoRoundedIcon className={classes.infoBoxIcon} />
 
@@ -99,110 +98,115 @@ export const Security: React.FC = () => {
                 </div>
               </div>
             )
-            : (
-              <>
-                <TextField
-                  className={classes.inputFields}
-                  fullWidth
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position='end'>
-                        <IconButton
-                          onClick={(event) => handleShowPassword(event, 0)}
-                        >
-                          {
-                            showPassword[0]
-                              ? <VisibilityRoundedIcon />
-                              : <VisibilityOffRoundedIcon />
-                          }
-                        </IconButton>
-                      </InputAdornment>
-                    ),
-                  }}
-                  label={t("profileTab.securitySubTab.fieldLabels.currentPasswordFieldLabel")}
-                  margin='dense'
-                  name='currentPassword'
-                  onChange={(changeEvent) => handlePasswordChanges(changeEvent, 0)}
-                  type={showPassword[0] ? "text" : "password"}
-                  value={providedPasswords[0]}
-                  variant='outlined'
-                />
+              : (
+                <>
+                  <TextField
+                    fullWidth
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position='end'>
+                          <IconButton
+                            onClick={(event) => handleShowPassword(event, 0)}
+                          >
+                            {
+                              showPassword[0]
+                                ? <VisibilityRoundedIcon />
+                                : <VisibilityOffRoundedIcon />
+                            }
+                          </IconButton>
+                        </InputAdornment>
+                      ),
+                    }}
+                    label={t("profileTab.securitySubTab.fieldLabels.currentPasswordFieldLabel")}
+                    margin='dense'
+                    name='currentPassword'
+                    onChange={(changeEvent) => handlePasswordChanges(changeEvent, 0)}
+                    type={showPassword[0] ? "text" : "password"}
+                    value={providedPasswords[0]}
+                    variant='outlined'
+                  />
 
-                <TextField
-                  className={classes.inputFields}
-                  error={
-                    providedPasswords[1].length === 0
-                      ? false
-                      : (
-                        providedPasswords[0] === providedPasswords[1]
-                          ? true
+                  <Box mb={3} mt={1.5}>
+                    <TextField
+                      fullWidth
+                      error={
+                        providedPasswords[1].length === 0
+                          ? false
                           : (
-                            !isValidPass(providedPasswords[1])
+                            providedPasswords[0] === providedPasswords[1]
+                              ? true
+                              : (
+                                !isValidPass(providedPasswords[1])
+                              )
                           )
-                      )
-                  }
-                  fullWidth
-                  helperText={
-                    providedPasswords[1].length === 0
-                      ? ""
-                      : (
-                        providedPasswords[0] === providedPasswords[1]
-                          ? t("profileTab.securitySubTab.errorMessages.samePasswordErrorMessage")
+                      }
+                      helperText={
+                        providedPasswords[1].length === 0
+                          ? ""
                           : (
-                            isValidPass(providedPasswords[1])
-                              ? ""
-                              : t("profileTab.securitySubTab.errorMessages.tooWeakPasswordErrorMessage")
+                            providedPasswords[0] === providedPasswords[1]
+                              ? t("profileTab.securitySubTab.errorMessages.samePasswordErrorMessage")
+                              : (
+                                isValidPass(providedPasswords[1])
+                                  ? ""
+                                  : t("profileTab.securitySubTab.errorMessages.tooWeakPasswordErrorMessage")
+                              )
                           )
-                      )
-                  }
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position='end'>
-                        <IconButton
-                          onClick={(event) => handleShowPassword(event, 1)}
-                        >
-                          {
-                            showPassword[1]
-                              ? <VisibilityRoundedIcon />
-                              : <VisibilityOffRoundedIcon />
-                          }
-                        </IconButton>
-                      </InputAdornment>
-                    ),
-                  }}
-                  label={t("profileTab.securitySubTab.fieldLabels.newPasswordFieldLabel")}
-                  margin='dense'
-                  name='newPassword'
-                  onChange={(changeEvent) => handlePasswordChanges(changeEvent, 1)}
-                  type={showPassword[1] ? "text" : "password"}
-                  value={providedPasswords[1]}
-                  variant='outlined'
-                />
+                      }
+                      InputProps={{
+                        endAdornment: (
+                          <InputAdornment position='end'>
+                            <IconButton
+                              onClick={(event) => handleShowPassword(event, 1)}
+                            >
+                              {
+                                showPassword[1]
+                                  ? <VisibilityRoundedIcon />
+                                  : <VisibilityOffRoundedIcon />
+                              }
+                            </IconButton>
+                          </InputAdornment>
+                        ),
+                      }}
+                      label={t("profileTab.securitySubTab.fieldLabels.newPasswordFieldLabel")}
+                      margin='dense'
+                      name='newPassword'
+                      onChange={(changeEvent) => handlePasswordChanges(changeEvent, 1)}
+                      type={showPassword[1] ? "text" : "password"}
+                      value={providedPasswords[1]}
+                      variant='outlined'
+                    />
+                  </Box>
 
-                <div className={classes.actionsContainer}>
                   <Button
-                    className={
-                      (providedPasswords[0] !== providedPasswords[1]) &&
-                (providedPasswords[0].length && providedPasswords[1].length) &&
-                (isValidPass(providedPasswords[1]))
-                        ? classes.enabledUpdatePasswordButton
-                        : classes.disabledUpdatePasswordButton
+                    variant="contained"
+                    color="primary"
+                    size="large"
+                    disableElevation
+                    disabled={
+                      !!((providedPasswords[0] !== providedPasswords[1]) &&
+                              (providedPasswords[0].length && providedPasswords[1].length) &&
+                              isValidPass(providedPasswords[1]))
                     }
                     onClick={handlePasswordChangeRequest}
                   >
                     {t("profileTab.securitySubTab.buttonLabels.updatePasswordButtonLabel")}
                   </Button>
-                </div>
-              </>
-            )
-        }
+                </>
+              )
+            }
+          </Grid>
 
-        <hr className={classes.sectionSeparator} />
+          <Grid item xs />
+        </Grid>
+
+        {/* FIXME: find what this is */}
+        {/* <hr className={classes.sectionSeparator} />
 
         <p className={classes.userActivityTitle}>
           {t("profileTab.securitySubTab.userActivityTitle")}
-        </p>
-      </section>
+        </p> */}
+      </Container>
     </main>
   );
 };
--- src/pages/Security/styles.ts
+++ src/pages/Security/styles.ts
@@ -1,62 +1,6 @@
 import { makeStyles } from "@apisuite/fe-base";
 
 export default makeStyles((theme) => ({
-  actionsContainer: {
-    display: "flex",
-    marginBottom: "65px",
-    marginTop: "10px",
-  },
-
-  disabledUpdatePasswordButton: {
-    backgroundColor: theme.palette.secondary.main,
-    border: `1px solid ${theme.palette.secondary.main}`,
-    borderRadius: theme.shape.borderRadius,
-    color: `${theme.palette.primary.contrastText} !important`,
-    cursor: "none",
-    fontSize: "16px",
-    fontWeight: 500,
-    height: "40px",
-    marginRight: "15px",
-    opacity: 0.5,
-    padding: "12px 20px",
-    pointerEvents: "none",
-    textDecoration: "none",
-
-    "&:hover": {
-      backgroundColor: theme.palette.secondary.main,
-    },
-  },
-
-  enabledUpdatePasswordButton: {
-    backgroundColor: theme.palette.secondary.main,
-    border: `1px solid ${theme.palette.secondary.main}`,
-    borderRadius: theme.shape.borderRadius,
-    color: `${theme.palette.primary.contrastText} !important`,
-    cursor: "pointer",
-    fontSize: "16px",
-    fontWeight: 500,
-    height: "40px",
-    marginRight: "15px",
-    padding: "12px 20px",
-    textDecoration: "none",
-
-    "&:hover": {
-      backgroundColor: theme.palette.secondary.main,
-    },
-  },
-
-  forgotPasswordButton: {
-    backgroundColor: theme.palette.background.paper,
-    border: `1px solid ${theme.palette.label}`,
-    borderRadius: theme.shape.borderRadius,
-    color: `${theme.palette.action.active} !important`,
-    fontSize: "16px",
-    fontWeight: 500,
-    height: "40px",
-    padding: "12px 20px",
-    textDecoration: "none",
-  },
-
   infoBox: {
     alignItems: "center",
     backgroundColor: theme.palette.info.light,
@@ -80,75 +24,6 @@ export default makeStyles((theme) => ({
     fontWeight: 400,
     lineHeight: "18px",
     margin: "0px 0px 5px 2.5px",
-  },
-
-  inputFields: {
-    marginBottom: "20px",
-    marginTop: "20px",
-    maxWidth: "500px",
-    width: "100%",
-
-    // Text field's label styles
-    "& > label": {
-      color: theme.palette.label,
-    },
-
-    "& label.Mui-focused": {
-      color: `${theme.palette.action.focus} !important`,
-    },
-
-    // Text field's input outline styles
-    "& .MuiOutlinedInput-root": {
-      "&:hover fieldset": {
-        borderColor: `${theme.palette.grey[300]} !important`,
-      },
-
-      "&.Mui-focused fieldset": {
-        borderColor: `${theme.palette.action.focus} !important`,
-      },
-    },
-
-    // Text field's input text styles
-    "& .MuiInputBase-root": {
-      "& .MuiInputBase-input": {
-        color: theme.palette.action.active,
-      },
-    },
-  },
-
-  sectionSeparator: {
-    border: `1px solid ${theme.palette.grey[200]}`,
-    borderRadius: theme.shape.borderRadius,
-    marginBottom: "25px",
-    maxWidth: "900px",
-    width: "100%",
-  },
-
-  securityTitle: {
-    color: theme.palette.secondary.main,
-    fontSize: "32px",
-    fontWeight: 300,
-    marginBottom: "12px",
-  },
-
-  securitySubtitle: {
-    color: theme.palette.text.primary,
-    fontSize: "16px",
-    fontWeight: 300,
-    marginBottom: "55px",
-  },
-
-  updatePasswordContainer: {
-    margin: "0px auto",
-    maxWidth: "900px",
-    width: "100%",
-  },
-
-  updatePasswordTitle: {
-    color: theme.palette.secondary.main,
-    fontSize: "16px",
-    fontWeight: 500,
-    marginBottom: "10px",
   },
 
   userActivityTitle: {
