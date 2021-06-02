a39f5c48f071b0e8e8f041e55b165f886d064056
Bruno Morgado <mstrk512@gmail.com>
2021-06-02 11:37:48 +0100

    style(Notice): align changes

--- src/components/Notice/styles.ts
+++ src/components/Notice/styles.ts
@@ -3,19 +3,16 @@ import { makeStyles } from "@apisuite/fe-base";
 export default makeStyles((theme) => ({
   noticeContentsContainer: {
     backgroundColor: theme.palette.info.light,
-    border: `1px solid ${theme.palette.info.light}`,
     borderRadius: `${theme.shape.borderRadius}px`,
     display: "flex",
-    alignItems: "center",
     padding: 12,
     width: "100%",
+    alignItems: "center",
   },
 
   noticeIcon: {
-    alignItems: "center",
     color: theme.palette.info.main,
-    display: "flex",
-    justifyContent: "center",
-    marginRight: "10px",
+    marginRight: 10,
+    alignSelf: "flex-start",
   },
 }));
